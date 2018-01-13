af7b689ee64a16344651fa1488f6743efe8eb34d
Deep-09 <deepbhasme@gmail.com>
2018-01-13 13:01:48 +0530

    create snapshot
    snapshot name asked by bot

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -109,7 +109,7 @@ namespace Microsoft.Bot.Sample.LuisBot
         [LuisIntent("Greeting")]
         public async Task GreetingIntent(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync($"Hello! I am your IT Service Desk Virtual Assistant *Robin* :) . I can help you with IT service related issues and requests. How may I help you today?");
+            await context.PostAsync($"Hello! I am your IT Service Desk Virtual Assistant *Maggie* :) . I can help you with IT service related issues and requests. How may I help you today?");
         }
 
         [LuisIntent("Cancel")]
@@ -317,16 +317,73 @@ namespace Microsoft.Bot.Sample.LuisBot
         }
 
         [LuisIntent("Add Snapshot")]
-        public async Task AddSnapshotIntent(IDialogContext context, LuisResult result)
+        public async Task AddSnapshotIntent_Test(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("Please provide VCenter:");
-
+            PromptDialog.Text(context, ResumeAfterSnapNameUnlockClarification, "Pardon me I didn't get your Snapshot Name there :)... I'm hoping you can help me with it..");
         }
+        private async Task ResumeAfterSnapNameUnlockClarification(IDialogContext context, IAwaitable<string> result)
+        {
+            sname = await result;
+            var client = new RestClient("http://96a7bf35.ngrok.io/aeengine/rest/authenticate");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("postman-token", "ea502694-bf8a-9c2e-e27b-8082381ce137");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
+            request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\naishwarya\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\nPune@123\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            string jsonresult;
+            jsonresult = response.Content;
+            var myDetails = JsonConvert.DeserializeObject<MyDetail>(jsonresult);
+            string token = myDetails.sessionToken;
+            var request1 = new RestRequest("http://96a7bf35.ngrok.io/aeengine/rest/execute", Method.POST);
+            request1.AddHeader("X-session-token", token);
+
+            JavaScriptSerializer serialiser = new JavaScriptSerializer();
+            List<AutomationParameter> ListAutomationField = new List<AutomationParameter>();
+
+            AutomationParameter parameter1 = new AutomationParameter();
+            parameter1.name = "snapshotname";
+            parameter1.value = sname;
+            parameter1.type = "String";
+            parameter1.order = 1;
+            parameter1.secret = false;
+            parameter1.optional = false;
+            parameter1.displayName = "snapshotname";
+            parameter1.extension = null;
+            parameter1.poolCredential = false;
+
+            ListAutomationField.Add(parameter1);
+
+            Guid temp = Guid.NewGuid();
+
+            RootAutomation AutoRoot = new RootAutomation();
+            AutoRoot.orgCode = "ACTIVEDIREC";
+            AutoRoot.workflowName = "createSnap";
+            AutoRoot.userId = "Aishwarya Chaudhary";
+            AutoRoot.@params = ListAutomationField;
+            AutoRoot.sourceId = temp.ToString();
+            AutoRoot.source = "AutomationEdge HelpDesk";
+            AutoRoot.responseMailSubject = null;
+            string json = serialiser.Serialize(AutoRoot);
+
+
+
+            //string body = "{\"orgCode\":\"FUSION\",\"workflowName\":\"Software Installation\",\"userId\":\"Admin Fusion\",\"sourceId\":\"SID_5b-912-21f4-88-880eb-8a0b-91\",\"source\":\"AutomationEdge HelpDesk\",\"responseMailSubject\":\"null\",\"params\":[{\"name\":\"software\",\"value\":\"JDK\",\"type\":\"String\",\"order\":1,\"secret\":false,\"optional\":false,\"defaultValue\":null,\"displayName\":\"Incident Number\",\"extension\":null,\"poolCredential\":false},{\"name\":\"slackChannel\",\"value\":\"fdgvdfg\",\"type\":\"String\",\"order\":2,\"secret\":false,\"optional\":false,\"defaultValue\":null,\"displayName\":\"Slack Channel\",\"extension\":null,\"poolCredential\":false}]}";
+            //var json = JsonConvert.SerializeObject(body);
+            request1.AddHeader("content-type", "application/json");
+            request1.AddParameter("application/json", json, ParameterType.RequestBody);
+            request1.RequestFormat = DataFormat.Json;
+            IRestResponse response1 = client.Execute(request1);
+
+            await context.PostAsync($"I will snapshot snapshot {sname} as soon as possible... Visit me again whenever you need my help. Have a great day.");
+        }
+
+
 
         [LuisIntent("Unlock AD Ask")]
         public async Task UnlockADIntent_Test(IDialogContext context, LuisResult result)
         {
-            PromptDialog.Text(context, ResumeAfterSamNameUnlockClarification, "Pardon me I didn't get your Sam account name there :)... I'm hoping you can help me with it..");
+            PromptDialog.Text(context, ResumeAfterSamNameUnlockClarification, "Pardon me I didn't get your Sam Account Name there :)... I'm hoping you can help me with it..");
         }
 
         private async Task ResumeAfterSamNameUnlockClarification(IDialogContext context, IAwaitable<string> result)
@@ -391,7 +448,7 @@ namespace Microsoft.Bot.Sample.LuisBot
         [LuisIntent("Add Account")]
         public async Task AddAccount(IDialogContext context, LuisResult result)
         {
-            PromptDialog.Text(context, ResumeAfterOrgNameClarification, "Please give me your organization unit ");
+            PromptDialog.Text(context, ResumeAfterOrgNameClarification, "Give me your organization unit please ");
         }
 
         private async Task ResumeAfterOrgNameClarification(IDialogContext context, IAwaitable<string> result)
@@ -411,7 +468,7 @@ namespace Microsoft.Bot.Sample.LuisBot
         private async Task ResumeAfterUserNameClarification(IDialogContext context, IAwaitable<string> result)
         {
             uname = await result;
-            PromptDialog.Text(context, ResumeAfterDispNameClarification, "What name would you like on display ");
+            PromptDialog.Text(context, ResumeAfterDispNameClarification, "What name you would like on display?");
             //await context.PostAsync($"You entered {sname}.");
         }
 
