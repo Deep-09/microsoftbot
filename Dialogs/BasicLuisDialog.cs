50903e872de63597b182aab968887839b6ce09f3
Deep-09 <deepbhasme@gmail.com>
2018-01-15 20:42:55 +0530

    working
    working

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -258,7 +258,13 @@ namespace Microsoft.Bot.Sample.LuisBot
         [LuisIntent("Add Snapshot")]
         public async Task AddSnapshotIntent_Test(IDialogContext context, LuisResult result)
         {
-            PromptDialog.Text(context, ResumeAfterSnapNameUnlockClarification, "Pardon me I didn't get your snapshot name there :)... I'm hoping you can help me with it..");
+            PromptDialog.Text(context, ResumeAfterVMNameClarification, "Please give me yourVM name ");
+        }
+        private async Task ResumeAfterVMNameClarification(IDialogContext context, IAwaitable<string> result)
+        {
+            VM_Name = await result;
+            PromptDialog.Text(context, ResumeAfterSnapNameUnlockClarification, "I didn't get your snapshot name :)... I'm hoping you can help me with it..");
+            //await context.PostAsync($"You entered {sname}.");
         }
         private async Task ResumeAfterSnapNameUnlockClarification(IDialogContext context, IAwaitable<string> result)
         {
@@ -281,17 +287,30 @@ namespace Microsoft.Bot.Sample.LuisBot
             List<AutomationParameter> ListAutomationField = new List<AutomationParameter>();
 
             AutomationParameter parameter1 = new AutomationParameter();
-            parameter1.name = "snapshotname";
+            parameter1.name = "VM_Name";
             parameter1.value = sname;
             parameter1.type = "String";
             parameter1.order = 1;
             parameter1.secret = false;
             parameter1.optional = false;
-            parameter1.displayName = "snapshotname";
+            parameter1.displayName = "VM_Name";
             parameter1.extension = null;
             parameter1.poolCredential = false;
 
             ListAutomationField.Add(parameter1);
+
+            AutomationParameter parameter2 = new AutomationParameter();
+            parameter2.name = "snapshotname";
+            parameter2.value = sname;
+            parameter2.type = "String";
+            parameter2.order = 2;
+            parameter2.secret = false;
+            parameter2.optional = false;
+            parameter2.displayName = "snapshotname";
+            parameter2.extension = null;
+            parameter2.poolCredential = false;
+
+            ListAutomationField.Add(parameter2);
 
             Guid temp = Guid.NewGuid();
 
@@ -319,74 +338,15 @@ namespace Microsoft.Bot.Sample.LuisBot
 
 
 
-        [LuisIntent("Unlock AD")]
-        public async Task UnlockADIntent_Test(IDialogContext context, IAwaitable<string> result)
+        [LuisIntent("Unlock AD Ask")]
+        public async Task UnlockADIntent_Test(IDialogContext context, LuisResult result)
         {
-            sname = await result;
-            if(sname != null)
-            {
-                var client = new RestClient("http://96a7bf35.ngrok.io/aeengine/rest/authenticate");
-                var request = new RestRequest(Method.POST);
-                request.AddHeader("postman-token", "ea502694-bf8a-9c2e-e27b-8082381ce137");
-                request.AddHeader("cache-control", "no-cache");
-                request.AddHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
-                request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\naishwarya\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\nPune@123\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
-                IRestResponse response = client.Execute(request);
-                string jsonresult;
-                jsonresult = response.Content;
-                var myDetails = JsonConvert.DeserializeObject<MyDetail>(jsonresult);
-                string token = myDetails.sessionToken;
-                var request1 = new RestRequest("http://96a7bf35.ngrok.io/aeengine/rest/execute", Method.POST);
-                request1.AddHeader("X-session-token", token);
-
-                JavaScriptSerializer serialiser = new JavaScriptSerializer();
-                List<AutomationParameter> ListAutomationField = new List<AutomationParameter>();
-
-                AutomationParameter parameter1 = new AutomationParameter();
-                parameter1.name = "Sam_Account_Name";
-                parameter1.value = sname;
-                parameter1.type = "String";
-                parameter1.order = 1;
-                parameter1.secret = false;
-                parameter1.optional = false;
-                parameter1.displayName = "Sam_Account_Name";
-                parameter1.extension = null;
-                parameter1.poolCredential = false;
-
-                ListAutomationField.Add(parameter1);
-
-                Guid temp = Guid.NewGuid();
-
-                RootAutomation AutoRoot = new RootAutomation();
-                AutoRoot.orgCode = "ACTIVEDIREC";
-                AutoRoot.workflowName = "UnlockAD";
-                AutoRoot.userId = "Aishwarya Chaudhary";
-                AutoRoot.@params = ListAutomationField;
-                AutoRoot.sourceId = temp.ToString();
-                AutoRoot.source = "AutomationEdge HelpDesk";
-                AutoRoot.responseMailSubject = null;
-                string json = serialiser.Serialize(AutoRoot);
-
-
-
-                //string body = "{\"orgCode\":\"FUSION\",\"workflowName\":\"Software Installation\",\"userId\":\"Admin Fusion\",\"sourceId\":\"SID_5b-912-21f4-88-880eb-8a0b-91\",\"source\":\"AutomationEdge HelpDesk\",\"responseMailSubject\":\"null\",\"params\":[{\"name\":\"software\",\"value\":\"JDK\",\"type\":\"String\",\"order\":1,\"secret\":false,\"optional\":false,\"defaultValue\":null,\"displayName\":\"Incident Number\",\"extension\":null,\"poolCredential\":false},{\"name\":\"slackChannel\",\"value\":\"fdgvdfg\",\"type\":\"String\",\"order\":2,\"secret\":false,\"optional\":false,\"defaultValue\":null,\"displayName\":\"Slack Channel\",\"extension\":null,\"poolCredential\":false}]}";
-                //var json = JsonConvert.SerializeObject(body);
-                request1.AddHeader("content-type", "application/json");
-                request1.AddParameter("application/json", json, ParameterType.RequestBody);
-                request1.RequestFormat = DataFormat.Json;
-                IRestResponse response1 = client.Execute(request1);
-
-                await context.PostAsync($"I will unlock account for {sname} as soon as possible... Visit me again whenever you need my help. Have a great day :)");
-            }
-            else
-            {
-                PromptDialog.Text(context, ResumeAfterSamNameUnlockClarification, "Pardon me I didn't get your sam account name there :)... I'm hoping you can help me with it..");
-            }
-            
+            PromptDialog.Text(context, ResumeAfterSamNameUnlockClarification, "Pardon me I didn't get your sam account name there :)... I'm hoping you can help me with it..");
         }
-
+        
         private async Task ResumeAfterSamNameUnlockClarification(IDialogContext context, IAwaitable<string> result)
         {
+            sname = await result;
             var client = new RestClient("http://96a7bf35.ngrok.io/aeengine/rest/authenticate");
             var request = new RestRequest(Method.POST);
             request.AddHeader("postman-token", "ea502694-bf8a-9c2e-e27b-8082381ce137");
@@ -634,10 +594,10 @@ namespace Microsoft.Bot.Sample.LuisBot
         private async Task ResumeAfterTemplateNameClarification(IDialogContext context, IAwaitable<string> result)
         {
             Template_Name = await result;
-            PromptDialog.Text(context, ResumeAfterVMNameClarification, "And give this VM a name of your choice ");
+            PromptDialog.Text(context, ResumeAfterVMName1Clarification, "And give this VM a name of your choice ");
             //await context.PostAsync($"I see you want to order {food}.");
         }
-        private async Task ResumeAfterVMNameClarification(IDialogContext context, IAwaitable<string> result)
+        private async Task ResumeAfterVMName1Clarification(IDialogContext context, IAwaitable<string> result)
         {
             VM_Name = await result;
 
