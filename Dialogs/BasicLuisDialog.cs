e0fab6769af9a7cc92fb205c3431d0856fa296b0
Deep-09 <deepbhasme@gmail.com>
2018-01-22 12:35:31 +0530

    test

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -93,7 +93,60 @@ namespace Microsoft.Bot.Sample.LuisBot
                 if (!string.IsNullOrEmpty(state.SamAccountName))
                 {
                     message += $"I will unlock account for  {state.SamAccountName} as soon as possible...";
+                   
+                    var client = new RestClient("http://96a7bf35.ngrok.io/aeengine/rest/authenticate");
+                    var request = new RestRequest(Method.POST);
+                    request.AddHeader("postman-token", "ea502694-bf8a-9c2e-e27b-8082381ce137");
+                    request.AddHeader("cache-control", "no-cache");
+                    request.AddHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
+                    request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\naishwarya\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\nPune@123\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
+                    IRestResponse response = client.Execute(request);
+                    string jsonresult;
+                    jsonresult = response.Content;
+                    var myDetails = JsonConvert.DeserializeObject<MyDetail>(jsonresult);
+                    string token = myDetails.sessionToken;
+                    var request1 = new RestRequest("http://96a7bf35.ngrok.io/aeengine/rest/execute", Method.POST);
+                    request1.AddHeader("X-session-token", token);
 
+                    JavaScriptSerializer serialiser = new JavaScriptSerializer();
+                    List<AutomationParameter> ListAutomationField = new List<AutomationParameter>();
+
+                    AutomationParameter parameter1 = new AutomationParameter();
+                    parameter1.name = "Sam_Account_Name";
+                    parameter1.value = state.SamAccountName\;
+                    parameter1.type = "String";
+                    parameter1.order = 1;
+                    parameter1.secret = false;
+                    parameter1.optional = false;
+                    parameter1.displayName = "Sam_Account_Name";
+                    parameter1.extension = null;
+                    parameter1.poolCredential = false;
+
+                    ListAutomationField.Add(parameter1);
+
+                    Guid temp = Guid.NewGuid();
+
+                    RootAutomation AutoRoot = new RootAutomation();
+                    AutoRoot.orgCode = "ACTIVEDIREC";
+                    AutoRoot.workflowName = "UnlockAD";
+                    AutoRoot.userId = "Aishwarya Chaudhary";
+                    AutoRoot.@params = ListAutomationField;
+                    AutoRoot.sourceId = temp.ToString();
+                    AutoRoot.source = "AutomationEdge HelpDesk";
+                    AutoRoot.responseMailSubject = null;
+                    string json = serialiser.Serialize(AutoRoot);
+
+
+
+                    //string body = "{\"orgCode\":\"FUSION\",\"workflowName\":\"Software Installation\",\"userId\":\"Admin Fusion\",\"sourceId\":\"SID_5b-912-21f4-88-880eb-8a0b-91\",\"source\":\"AutomationEdge HelpDesk\",\"responseMailSubject\":\"null\",\"params\":[{\"name\":\"software\",\"value\":\"JDK\",\"type\":\"String\",\"order\":1,\"secret\":false,\"optional\":false,\"defaultValue\":null,\"displayName\":\"Incident Number\",\"extension\":null,\"poolCredential\":false},{\"name\":\"slackChannel\",\"value\":\"fdgvdfg\",\"type\":\"String\",\"order\":2,\"secret\":false,\"optional\":false,\"defaultValue\":null,\"displayName\":\"Slack Channel\",\"extension\":null,\"poolCredential\":false}]}";
+                    //var json = JsonConvert.SerializeObject(body);
+                    request1.AddHeader("content-type", "application/json");
+                    request1.AddParameter("application/json", json, ParameterType.RequestBody);
+                    request1.RequestFormat = DataFormat.Json;
+                    IRestResponse response1 = client.Execute(request1);
+
+                    //await context.PostAsync($"You reached {result.Intents[0].Intent} resonse is {response1.Content} .");
+                    await context.PostAsync($"{response1.Content}");
                 }
                 await context.PostAsync(message);
 
@@ -110,60 +163,7 @@ namespace Microsoft.Bot.Sample.LuisBot
         {
             try
             {
-                var san = await result;
-                var client = new RestClient("http://96a7bf35.ngrok.io/aeengine/rest/authenticate");
-                var request = new RestRequest(Method.POST);
-                request.AddHeader("postman-token", "ea502694-bf8a-9c2e-e27b-8082381ce137");
-                request.AddHeader("cache-control", "no-cache");
-                request.AddHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW");
-                request.AddParameter("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW", "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\naishwarya\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\nPune@123\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--", ParameterType.RequestBody);
-                IRestResponse response = client.Execute(request);
-                string jsonresult;
-                jsonresult = response.Content;
-                var myDetails = JsonConvert.DeserializeObject<MyDetail>(jsonresult);
-                string token = myDetails.sessionToken;
-                var request1 = new RestRequest("http://96a7bf35.ngrok.io/aeengine/rest/execute", Method.POST);
-                request1.AddHeader("X-session-token", token);
-
-                JavaScriptSerializer serialiser = new JavaScriptSerializer();
-                List<AutomationParameter> ListAutomationField = new List<AutomationParameter>();
-
-                AutomationParameter parameter1 = new AutomationParameter();
-                parameter1.name = "Sam_Account_Name";
-                parameter1.value = san;
-                parameter1.type = "String";
-                parameter1.order = 1;
-                parameter1.secret = false;
-                parameter1.optional = false;
-                parameter1.displayName = "Sam_Account_Name";
-                parameter1.extension = null;
-                parameter1.poolCredential = false;
-
-                ListAutomationField.Add(parameter1);
-
-                Guid temp = Guid.NewGuid();
-
-                RootAutomation AutoRoot = new RootAutomation();
-                AutoRoot.orgCode = "ACTIVEDIREC";
-                AutoRoot.workflowName = "UnlockAD";
-                AutoRoot.userId = "Aishwarya Chaudhary";
-                AutoRoot.@params = ListAutomationField;
-                AutoRoot.sourceId = temp.ToString();
-                AutoRoot.source = "AutomationEdge HelpDesk";
-                AutoRoot.responseMailSubject = null;
-                string json = serialiser.Serialize(AutoRoot);
-
-                
-
-                //string body = "{\"orgCode\":\"FUSION\",\"workflowName\":\"Software Installation\",\"userId\":\"Admin Fusion\",\"sourceId\":\"SID_5b-912-21f4-88-880eb-8a0b-91\",\"source\":\"AutomationEdge HelpDesk\",\"responseMailSubject\":\"null\",\"params\":[{\"name\":\"software\",\"value\":\"JDK\",\"type\":\"String\",\"order\":1,\"secret\":false,\"optional\":false,\"defaultValue\":null,\"displayName\":\"Incident Number\",\"extension\":null,\"poolCredential\":false},{\"name\":\"slackChannel\",\"value\":\"fdgvdfg\",\"type\":\"String\",\"order\":2,\"secret\":false,\"optional\":false,\"defaultValue\":null,\"displayName\":\"Slack Channel\",\"extension\":null,\"poolCredential\":false}]}";
-                //var json = JsonConvert.SerializeObject(body);
-                request1.AddHeader("content-type", "application/json");
-                request1.AddParameter("application/json", json, ParameterType.RequestBody);
-                request1.RequestFormat = DataFormat.Json;
-                IRestResponse response1 = client.Execute(request1);
-
-                //await context.PostAsync($"You reached {result.Intents[0].Intent} resonse is {response1.Content} .");
-                await context.PostAsync($"{response1.Content}");
+               
                 await context.PostAsync("Visit me again whenever you need me .Have a great day :)");
 
            
