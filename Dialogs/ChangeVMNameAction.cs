631af7799b5103440ecf56fb590e6fe0301b0d81
Deep-09 <deepbhasme@gmail.com>
2018-02-01 15:43:55 +0530

    Change Username & VM Name

---
+++ Dialogs/ChangeVMNameAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Bot.Sample.LuisBot;
+using Microsoft.Cognitive.LUIS.ActionBinding;
+
+namespace LuisBot.Dialogs
+{
+    [Serializable]
+    [LuisActionBinding("Change VM Name", FriendlyName = "Change VM Name")]
+    public class ChangeVMNameAction : BaseLuisContextualAction<CreatingSnapshotAction>
+    {
+        [Required(ErrorMessage = "Please provide a new VM Name")]
+        [LuisActionBindingParam(CustomType = "VM_Name")]
+        public string VM_Name { get; set; }
+
+        public override Task<object> FulfillAsync()
+        {
+            if (this.Context == null)
+            {
+                throw new InvalidOperationException("Action context not defined.");
+            }
+
+            this.Context.VM_Name = this.VM_Name;
+
+            return Task.FromResult((object)$"VM Name changed to {this.VM_Name}");
+        }
+    }
+}
