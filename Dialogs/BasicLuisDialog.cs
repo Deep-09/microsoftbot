ddfec3b8c1f5b9fcfb631e1881cc814bd187fa46
Deep-09 <deepbhasme@gmail.com>
2018-02-06 12:00:59 +0530

    Cancel intent

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -39,19 +39,25 @@ namespace Microsoft.Bot.Sample.LuisBot
         public async Task IntentGreetingHandlerAsync(IDialogContext context, object actionResult)
         {
             var message = context.MakeMessage();
-
             message.Text = actionResult != null ? actionResult.ToString() : "Cannot resolve your query";
-
             await context.PostAsync(message);
+        }
+
+
+        [LuisIntent("Cancel")]
+        public async Task IntentCancelHandlerAsync(IDialogContext context, object actionResult)
+        {
+            var message = context.MakeMessage();
+            message.Text = actionResult != null ? actionResult.ToString() : "Cannot resolve your query";
+            await context.PostAsync(message);
+            
         }
 
         [LuisIntent("Help")]
         public async Task IntentHelpHandlerAsync(IDialogContext context, object actionResult)
         {
             var message = context.MakeMessage();
-
             message.Text = actionResult != null ? actionResult.ToString() : "Cannot resolve your query";
-
             await context.PostAsync(message);
             await context.PostAsync("You may say something like this\n Add ad user\n Please unlock ad user\n Take a snap\n Add VM");
         }
@@ -60,9 +66,7 @@ namespace Microsoft.Bot.Sample.LuisBot
         public async Task IntentNoneHandlerAsync(IDialogContext context, object actionResult)
         {
             var message = context.MakeMessage();
-
             message.Text = actionResult != null ? actionResult.ToString() : "Cannot resolve your query";
-
             await context.PostAsync(message);
             await context.PostAsync("\nYou may say something like this\n Add ad user\n Please unlock ad user\n Take a snap\n Add VM");
         }
@@ -70,56 +74,35 @@ namespace Microsoft.Bot.Sample.LuisBot
         [LuisIntent("Creating Snapshot")]
         public async Task IntentActionResultHandlerAsync(IDialogContext context, object actionResult)
         {
-            
             var message = context.MakeMessage();
-
             message.Text = actionResult != null ? actionResult.ToString() : "Cannot resolve your query";
-
-            //await this.AECallAsync(context, actionResult);
-
             await context.PostAsync(message);
         }
 
         [LuisIntent("Create Virtual Machine​​")]
         public async Task IntentCreateVirtualMachine​​ActionResultHandlerAsync(IDialogContext context, object actionResult)
         {
-
             var message = context.MakeMessage();
-
             message.Text = actionResult != null ? actionResult.ToString() : "Cannot resolve your query";
-
-            //await this.AECallAsync(context, actionResult);
-
             await context.PostAsync(message);
         }
 
         [LuisIntent("Unlock AD Account")]
         public async Task IntentUnlockADAccountActionResultHandlerAsync(IDialogContext context, object actionResult)
         {
-
             var message = context.MakeMessage();
-
-            message.Text = actionResult != null ? actionResult.ToString() : "Cannot resolve your query";
-
-            //await this.AECallAsync(context, actionResult);
-
+            message.Text = actionResult != null ? actionResult.ToString() : "Cannot resolve your query";   
             await context.PostAsync(message);
         }
 
         [LuisIntent("Creating Active Directory User")]
         public async Task IntentCreatingActiveDirectoryUserActionResultHandlerAsync(IDialogContext context, object actionResult)
         {
-
             var message = context.MakeMessage();
-
             message.Text = actionResult != null ? actionResult.ToString() : "Cannot resolve your query";
-
-            //await this.AECallAsync(context, actionResult);
-
             await context.PostAsync(message);
         }
-
-        
+ 
     }
 
     public class MyDetail
---
+++ Dialogs/CancelAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Cognitive.LUIS.ActionBinding;
+
+namespace LuisBot.Dialogs
+{
+    [Serializable]
+    [LuisActionBinding("Cancel", FriendlyName = "Cancel")]
+    public class CancelAction : BaseLuisAction
+    {
+        public override Task<object> FulfillAsync()
+        {
+            return Task.FromResult((object)$"I canceled your action");
+        }
+    }
+}
