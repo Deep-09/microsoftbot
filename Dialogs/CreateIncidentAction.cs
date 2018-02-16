b33bb13ec8056f2b2d69388670e4682e6de4a1a7
Deep-09 <deepbhasme@gmail.com>
2018-02-16 13:05:31 +0530

    Rebuild

---
+++ Dialogs/CreateIncidentAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Cognitive.LUIS.ActionBinding;
+
+namespace LuisBot.Dialogs
+{
+    [Serializable]
+    [LuisActionBinding("Create Incident", FriendlyName = "Create Incident")]
+    public class CreateIncidentAction : BaseLuisAction
+    {
+        [Required(ErrorMessage = "Can you please elaborate ?")]
+        [LuisActionBindingParam(CustomType = "Description", Order = 1)]
+        //CustomType/BuiltinType = (Entity name in LUIS)
+        public string description { get; set; }
+
+
+        public override Task<object> FulfillAsync()
+        {
+            Dictionary<string, string> MyEntities = new Dictionary<string, string>();
+
+            MyEntities.Add("description", this.description);
+
+            CreateJSON createJSON = new CreateJSON();
+
+            createJSON.AECall(MyEntities, "Create Incident");
+
+            return Task.FromResult((object)$"I will create incident for  {this.description} as soon as possible... Visit me again whenever you need my help. Have a great day :)");
+        }
+    }
+}
